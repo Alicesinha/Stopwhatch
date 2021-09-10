64f8c04bf80ef4ed5878c349ec0ebafe74a2fb5f
William-Dias-Marcos <william.diasmarcos@gmail.com>
2021-09-09 22:05:36 -0300

    Part 02

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-
 var timer
 var hour = 0
 var minute = 0
 var seconds = 0
 
+var click = false
+
 function start(){
-    timer = window.setInterval( watch, 1000)
+
+    if (!click){
+    
+        timer = window.setInterval( function watch() {
+
+            seconds++
+
+            if (seconds == 60){
+                seconds = 0
+                minute++
+            }
+            if (minute == 60){
+                minute = 0
+                hour++
+            }
+
+            var stopWatch = hour + ":" + minute + ":" + seconds
+            document.getElementById("h1").innerText = stopWatch
+        
+        }, 1000)
+        click = true
+    }
 }
 
 function stop(){
     clearInterval(timer)
+    click = false
 }
 
 function reset(){
     clearInterval(timer)
+    click = false
     hour = 0
     minute = 0
     seconds = 0
     let h = document.getElementById("h1")
     h.innerText =  "00:00:00"
 }
-
-function watch() {
-   seconds++
-
-   if (seconds == 60){
-       seconds = 0
-       minute++
-   }
-   if (minute == 60){
-       minute = 0
-    hour++
-   }
-
-   var stopWatch = hour + ":" + minute + ":" + seconds
-   document.getElementById("h1").innerText = stopWatch
-}
-
