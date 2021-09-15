a7317dd822527665a07c42ad4f4393fa5d1e24d9
William-Dias-Marcos <william.diasmarcos@gmail.com>
2021-09-14 21:18:53 -0300

    Final Part

--- script.js
+++ script.js
@@ -22,12 +22,17 @@ function start(){
                 hour++
             }
 
-            var stopWatch = hour + ":" + minute + ":" + seconds
+            var stopWatch = number(hour) + ":" + number(minute) + ":" + number(seconds)
             document.getElementById("h1").innerText = stopWatch
         
         }, 1000)
         click = true
     }
+}
+function number(element) {
+    
+    return element < 10 ? '0' + element : element;
+
 }
 
 function stop(){
